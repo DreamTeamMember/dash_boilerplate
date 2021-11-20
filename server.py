069e8f9415ab88c9db00c9ac316910c44cacf2a2
DreamTeamMember <94527257+DreamTeamMember@users.noreply.github.com>
2021-11-20 16:57:46 +0000

    customize favicon and title

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from dash import Dash
 import dash_bootstrap_components as dbc
 from app import init_app
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], title="Boilerplate", suppress_callback_exceptions=True)
 init_app(app)
 
 if __name__ == '__main__':
